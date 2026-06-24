6c847ac00f0c2cc1880b51d297209b9e031e1eb6
SOM3-1 <dush.gowda@gmail.com>
2026-06-24 01:56:21 -0500

    Initial Commit

---
+++ EyeControlMac/EyeControlMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeControlMacApp.swift
+//  EyeControlMac
+//
+//  Created by Dushyanth N Gowda on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct EyeControlMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
